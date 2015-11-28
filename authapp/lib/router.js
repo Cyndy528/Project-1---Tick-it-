4fb6f27b4a6f6e0afc00f62b04a01267ee4633e0
Cynthia Salinas <csalinas123@gmail.com>
2015-11-28 11:53:05 -0800

    validation and dashboard

--- authapp/lib/router.js
+++ authapp/lib/router.js
@@ -1,16 +1,23 @@
+
 Router.route('/', function(){
 	//Login 
 	this.render('login'); 
 }); 
 
-Router.route('/login', function(){
-	var req = this.request; 
-	var res = this.response; 
-	res.end('hello from the server\n'); 	
-}, {where: 'server'}); 
-
 Router.route('/register', function(){
 	//Register Page
 	this.render('register'); 
 }); 
- 
+
+Router.route('/dashboard', function(){
+	//Dashboard Page
+	this.render('dashboard');
+
+Router.onBeforeAction(function() {
+  if (! Meteor.userId()) {
+    this.render('/');
+  } else {
+    this.next();
+  }
+});
+}); 
