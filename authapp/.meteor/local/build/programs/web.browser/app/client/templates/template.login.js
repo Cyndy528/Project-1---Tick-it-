c3c088430845616e1b5a719d97f07620c80b8fb8
Cynthia Salinas <csalinas123@gmail.com>
2015-11-30 14:35:26 -0800

    remove authapp code

--- authapp/.meteor/local/build/programs/web.browser/app/client/templates/template.login.js
+++
@@ -1,8 +0,0 @@
-(function(){
-Template.__checkName("login");
-Template["login"] = new Template("Template.login", (function() {
-  var view = this;
-  return HTML.Raw('<form class="form-signin">\n        <h2 class="form-signin-heading">Sign in</h2>\n\n        <label for="email" class="sr-only">Email address</label>\n\n        <input type="email" id="email" name="email" class="form-control" placeholder="Email address" required="" autofocus="">\n\n        <label for="password" class="sr-only">Password</label>\n\n        <input type="password" id="password" name="password" class="form-control" placeholder="Password" required="">\n\n        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>\n\n        <br>\n\n        <p>No Account? <a href="/register">Create An Account</a>\n\n        </p>\n\n      </form>');
-}));
-
-}).call(this);
--- authapp/.meteor/local/build/programs/web.browser/app/client/templates/template.navbar.js
+++
@@ -1,8 +0,0 @@
-(function(){
-Template.__checkName("navbar");
-Template["navbar"] = new Template("Template.navbar", (function() {
-  var view = this;
-  return HTML.Raw('<div class="header clearfix">\n        <nav>\n          <ul class="nav nav-pills pull-right">\n            <li role="presentation" class="active"><a href="/dashboard">Dashboard Home</a></li>\n            <li class="logout-bnt" role="presentation"><a href="#">Logout</a></li>\n\n          </ul>\n        </nav>\n        <h3 class="text-muted">Dashboard</h3>\n      </div>');
-}));
-
-}).call(this);
